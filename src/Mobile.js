b25b580b20747716b10e2e0cd0624102a45134f5
Arjun Singh <152479005+arjunsinghgithub@users.noreply.github.com>
2026-02-05 20:18:24 +0530

    Update Mobile.js
    alt and href attribute updated

--- src/Mobile.js
+++ src/Mobile.js
@@ -4,7 +4,7 @@ function Phone (props){
         <>
             <div id="container">
                 <div id="image">
-                    <img src={props.src} alt= {props.alt} height="200px" width="160px"/>
+                    <img src={props.src} alt= {props.alt} height="200px" width="160px" alt="mobile-picture"/>
                 </div>
                 <div id="details">
                     <h1>{props.name}</h1>
@@ -19,10 +19,10 @@ function Phone (props){
                 <div id="price-section">
                     <div>
                         <span id="price">&#8377;{props.price} </span>
-                        <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_9e47c1.png" height="20px"/>
+                        <img src="https://static-assets-web.flixcart.com/fk-p-linchpin-web/fk-cp-zion/img/fa_9e47c1.png" height="20px" alt="flipkart-Assured.png"/>
                     </div>
                     <div><span>Upto <strong>&#8377;{props.offOnExchange}</strong> Off on Exchange</span></div>
-                    <div><span><a href="#">Bank Offer</a></span></div>
+                    <div><span><a href="/bank-offer">Bank Offer</a></span></div>
                     
                 </div>
             </div>
